fc99679763b3a193c22293089c78e3a29a396f54
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-07-28 13:49:47 +0300

    Create MetalBlank.cs

---
+++ BlanksShop/Entities/MetalBlank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class MetalBlank: Product
+    {
+        public MetalBlank(int id, string name, Vector3 dimensions, Material material, double weight) : base(id, name, dimensions, material, weight)
+        {
+            
+        }
+
+        public MetalBlank(string name, Vector3 dimensions, Material material, double weight) : base(name, dimensions, material, weight)
+        {
+
+        }
+    }
+}
