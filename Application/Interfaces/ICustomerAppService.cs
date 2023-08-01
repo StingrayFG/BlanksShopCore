8c00c7eaf948b5ad9f606c909d97bf86a7322706
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-01 09:01:27 +0300

    Create ICustomerAppService.cs

---
+++ Application/Interfaces/ICustomerAppService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface ICustomerAppService : IAppService<Customer>
+    {
+        public void Add(string name, string phoneNumber, string password);
+        public void UpdateName(int id, string name);
+        
+    }
+}
