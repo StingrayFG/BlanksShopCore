ea67bf7c15bba86a44bb99873ca44b7a89c69d03
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-02 12:59:31 +0300

    added complex repos folder

---
+++ Infrastructure/Repositories/ComplexRepositories/MetalBlankRepository.cs
@@ -0,0 +1,90 @@
+﻿using Domain.Entities;
+using Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.EntitiesEF;
+
+namespace Infrastructure.Repositories
+{
+    public class MetalBlankRepository : IRepository<MetalBlank>
+    {
+        private IRepository<MetalBlankEF> _metalBlankRepository;
+        private IRepository<Material> _materialBlankRepository;
+
+
+        public MetalBlankRepository()
+        {
+            _metalBlankRepository = new Repository<MetalBlankEF>();
+            _materialBlankRepository = new Repository<Material>();
+        }
+
+        public MetalBlankRepository(IRepository<MetalBlankEF> metalBlankRepository, IRepository<Material> materialBlankRepository)
+        {
+            _metalBlankRepository = metalBlankRepository;
+            _materialBlankRepository = materialBlankRepository;
+        }
+
+        public void Add(MetalBlank metalBlank)
+        {
+            MetalBlankEF metalBlankEF = new MetalBlankEF(metalBlank);
+            _metalBlankRepository.Add(metalBlankEF);
+        }
+
+        public MetalBlank? GetByID(int id)
+        {
+            MetalBlankEF? metalBlankEF = _metalBlankRepository.GetByID(id);
+            if (metalBlankEF != null)
+            {
+                Material? material = _materialBlankRepository.GetByID(metalBlankEF.MaterialID);
+                MetalBlank metalBlank = metalBlankEF.Convert(material);
+                return metalBlank;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<MetalBlank>? GetAll()
+        {
+            List<MetalBlankEF>? metalBlanksEF = _metalBlankRepository.GetAll();
+            if (metalBlanksEF != null)
+            {
+                List<MetalBlank> res = new List<MetalBlank>();
+                foreach (MetalBlankEF metalBlankEF in metalBlanksEF)
+                {
+                    Material? material = _materialBlankRepository.GetByID(metalBlankEF.MaterialID);
+                    MetalBlank metalBlank = metalBlankEF.Convert(material);
+                    res.Add(metalBlank);
+                }
+                return res;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Update(MetalBlank metalBlank)
+        {
+            MetalBlankEF metalBlankEF = new MetalBlankEF(metalBlank);
+            _metalBlankRepository.Update(metalBlankEF);
+        }
+
+        public void Delete(MetalBlank metalBlank)
+        {
+            MetalBlankEF metalBlankEF = new MetalBlankEF(metalBlank);
+            _metalBlankRepository.Delete(metalBlankEF);
+        }
+
+        public void DeleteByID(int id)
+        {
+            _metalBlankRepository.DeleteByID(id);
+        }
+
+    }
+}
