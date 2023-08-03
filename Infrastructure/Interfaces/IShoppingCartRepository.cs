98b7cb407233d8a631eebac8f07f879cfd53dbbb
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-03 12:07:21 +0300

    added shopping cart repo

---
+++ Infrastructure/Interfaces/IShoppingCartRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IShoppingCartRepository<T> where T : ShoppingCart
+    {
+        public T? GetByID(int id);
+
+        public T? GetByCustomer(int id);
+        public List<T>? GetAll();
+        public void Add(int customerID, int productID);
+        public void DeleteByID(int id);
+
+    }
+}
