939e6943d09b1cbc1aa3f3782663f367411f3f4a
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-09 13:05:29 +0300

    completed order repo

---
+++ Infrastructure/EntitiesEF/OrderEF.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Infrastructure.EntitiesEF
+{
+    public class OrderEF: EntityBase
+    {
+        public int ID { get; set; }
+        public DateTime OrderCreationDate { get; set; }
+        public decimal? Price { get; set; }
+        public string? PaymentMethod { get; set; }
+        public int CustomerID { get; set; }
+
+        public OrderEF()
+        {
+
+        }
+
+        public OrderEF(Order order)
+        {
+            ID = order.ID;
+            OrderCreationDate = order.OrderCreationDate;
+            Price = order.Price;
+            PaymentMethod = order.PaymentMethod;
+            CustomerID = order.CustomerID;
+        }
+
+        public void Convert(Order order)
+        {
+            order.ID = ID;
+            order.OrderCreationDate = OrderCreationDate;
+            order.Price = Price;
+            order.PaymentMethod = PaymentMethod;
+            order.CustomerID = CustomerID;
+        }
+
+    }
+}
---
+++ Infrastructure/Repositories/ComplexRepositories/OrderRepository.cs
@@ -0,0 +1,79 @@
+﻿using Domain.Entities;
+using Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.EntitiesEF;
+
+namespace Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository<Order>
+    {
+        private IRepository<OrderEF> _orderRepository;
+        private IShoppingCartRepository<ShoppingCart> _shoppingCartRepository;
+
+
+        public OrderRepository()
+        {
+            _orderRepository = new Repository<OrderEF>();
+            _shoppingCartRepository = new ShoppingCartRepository();
+        }
+
+        public OrderRepository(IRepository<OrderEF> orderRepository, IShoppingCartRepository<ShoppingCart> shoppingCartRepository)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCartRepository = shoppingCartRepository;
+        }
+
+        public Order? GetByID(int id)
+        {
+            OrderEF? order = _orderRepository.GetByID(id);
+            Order res = new Order();
+            if (order != null)
+            {
+                order.Convert(res);
+            }
+            return res;
+        }
+
+        public List<Order>? GetAll()
+        {
+            List<OrderEF>? ordersEF = _orderRepository.GetAll();
+            List<Order> res = new List<Order>();
+            
+            foreach(OrderEF orderEF in ordersEF)
+            {
+                Order order = new Order();
+                orderEF.Convert(order);
+                res.Add(order);
+            }
+
+            return res;
+        }
+
+        public void Add(Order order)
+        {
+            _orderRepository.Add(new OrderEF(order));
+            _shoppingCartRepository.UpdateOrder(order.ShoppingCart.ID, order.ID);
+        }
+
+        public void Update(Order order)
+        {
+            OrderEF? orderEF = _orderRepository.GetByID(order.ID);
+            if (orderEF != null)
+            {
+                orderEF = new OrderEF(order);
+                _orderRepository.Update(orderEF);
+            }
+            
+        }
+
+        public void DeleteByID(int id)
+        {
+            _orderRepository.DeleteByID(id);
+        }
+    }
+}
