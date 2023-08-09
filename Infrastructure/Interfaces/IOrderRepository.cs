f6daf31c2dc06b20df56a28516046ff31e877b8d
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-09 13:08:57 +0300

    added order repo interface

---
+++ Infrastructure/Interfaces/IOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IOrderRepository<T>
+    {
+        public T? GetByID(int id);
+        public List<T>? GetAll();
+        public void Add(T entity);
+        public void Update(T entity);
+        public void DeleteByID(int id);
+
+    }
+}
