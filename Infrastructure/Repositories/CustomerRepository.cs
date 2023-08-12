15f485c6b596eba0f12f99cda4201fbfe78ada9b
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-12 14:16:47 +0300

    created ICustomerRepository interface, implemented GetByLogin

---
+++ Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.EntitiesEF;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository<Customer>
+    {
+        public Customer? GetByLogin(string login, string password)
+        {
+            return (from e in _dbContext.Set<Customer>() where (e.PhoneNumber == login) && (e.Password == password) select e).First();
+        }
+
+    }
+}
