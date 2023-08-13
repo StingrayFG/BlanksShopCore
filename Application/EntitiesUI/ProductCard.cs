3971df03dffc2ce43e0b7ff4481c57ff9c4f4887
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-13 06:33:11 +0300

    added ProductCart entity
    added a specific entity which simplifies the structure of the products catalog returned to UI

---
+++ Application/EntitiesUI/ProductCard.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.EntitiesUI
+{
+    public class ProductCard
+    {
+        public string Name { get; private set; }
+        public string MaterialName { get; private set; }
+        public List<Product> Products { get; private set; } = new List<Product>();
+
+        public ProductCard(string name, string materialName)
+        {
+            Name = name;
+            MaterialName = materialName;
+        }
+
+        public void AddProduct(Product product) 
+        {
+            Products.Add(product);  
+        }
+    }
+}
