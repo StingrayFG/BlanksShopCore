1655f78e82e4cfbd179ec64666dd5f2d0f22bd72
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-08-30 17:54:11 +0300

    added ProductType class

---
+++ BlanksShop/Entities/ProductType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProductType : EntityBase
+    {
+        public string Name { get; protected set; }
+        public string? Description { get; protected set; }
+
+        public ProductType() { }
+    }
+}
